4d15626d08c94fe091420f6274eedeb832d24de0
sid <Sydney.GendronSilva@stu.crc-lennox.qc.ca>
2026-09-17 12:18:55 -0400

    Created the Person class and the necessary elements.

---
+++ src/main/java/org/champlain/oop2/oop2a1f26/Person.java
@@ -0,0 +1,45 @@
+package org.champlain.oop2.oop2a1f26;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    public String aName;
+    public LocalDate aDOB;
+    public String aEmail;
+    public boolean aHasParkingPass;
+    public static List<Person> aPersons = new ArrayList<>();
+
+    public Person(String pName, LocalDate pDOB, String pEmail) {
+        this.aName = pName;
+        this.aDOB = pDOB;
+        this.aEmail = pEmail;
+        this.aHasParkingPass = false;
+        Person.aPersons.add(this);
+    }
+
+    public String getName() {
+        return this.aName;
+    }
+
+    public LocalDate getDOB() {
+        return this.aDOB;
+    }
+
+    public String getEmailAddress() {
+        return this.aEmail;
+    }
+
+    public boolean purchaseParkingPass() {
+        if (!this.aHasParkingPass) {
+            this.aHasParkingPass = true;
+        }
+
+        return aHasParkingPass;
+    }
+
+    public boolean isPurchasedParkingPass() {
+        return aHasParkingPass;
+    }
+}
